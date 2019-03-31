3716a7d3de0d0fcdcc383f377aef3b283223174e
doulfa <amadoulfa85@gmail.com>
2019-03-31 21:23:40 +0000

    Social Network

---
+++ social_network/helper.py
@@ -0,0 +1,42 @@
+def get_week_day_name(val_int):
+    if val_int == 0:
+        return 'Monday'
+    if val_int == 1:
+        return 'Tuesday'
+    if val_int == 2:
+        return 'Wednesday'
+    if val_int == 3:
+        return 'Thursday'
+    if val_int == 4:
+        return 'Friday'
+    if val_int == 5:
+        return 'Saturday'
+    if val_int == 6:
+        return 'Sunday'
+    
+def short_name(numb):         
+    if numb == 1:
+        return 'Jan'
+    if numb == 2:
+        return 'Feb'
+    if numb == 3:
+        return 'Mar'
+    if numb == 4:
+        return 'Apr'
+    if numb == 5:
+        return 'May'
+    if numb == 6:
+        return 'Jun'
+    if numb == 7:
+        return 'Jul'
+    if numb == 8:
+        return 'Aug'
+    if numb == 9:
+        return 'Sep'
+    if numb == 10:
+        return 'Oct'
+    if numb == 11:
+        return 'Nov',
+    if numb == 12:
+        return 'Dec'
+    
