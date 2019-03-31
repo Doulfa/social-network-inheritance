21f27aeedbe6f36c08e58d8ca8a93f264c4f893f
doulfa <amadoulfa85@gmail.com>
2019-03-31 21:27:40 +0000

    Social Network

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,23 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following =[]
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        list_follow = []
+        for user in self.following:
+            # list_follow += user.posts
+            for post in user.posts:
+                list_follow.append(post)
+        return list_follow
+    
 
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,71 @@
 from datetime import datetime
+from social_network.helper import get_week_day_name,short_name
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text =text
+        self.timestamp = timestamp
+        
+
+    def set_user(self, user):
+        self.user = user
+    
+
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super().__init__(text)
+        self.timestamp = timestamp 
+        self.user = None
+            
+    def __str__(self):
+        
+        timestamp = datetime.timestamp(self.timestamp) # convert timestamp to integer before passing below
+        dt_object = datetime.fromtimestamp(timestamp)
+        week_name=get_week_day_name(dt_object.weekday())
+        m=dt_object.month
+        s_m =short_name(m)
+        d = dt_object.day
+        y = dt_object.year
+        # '@{} {}: "{}"{}{}, {} {}, {}'.format(self.user.first_name,self.user.last_name,self.text,"\n\t",week_name,s_m,d,y)
+        return '@{} {}: "{}"{}{}, {} {}, {}'.format(self.user.first_name,self.user.last_name,self.text,"\n\t",week_name,s_m,d,y)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text)
+        self.image_url = image_url
+        self.timestamp = timestamp
 
+    def __str__(self):
+        
+        timestamp = datetime.timestamp(self.timestamp) # convert timestamp to integer before passing below
+        dt_object = datetime.fromtimestamp(timestamp)
+        week_name=get_week_day_name(dt_object.weekday())
+        m=dt_object.month
+        s_m =short_name(m)
+        d = dt_object.day
+        y = dt_object.year
+        
+        return '@{} {}: "{}"{}{}{}{}, {} {}, {}'.format(self.user.first_name,self.user.last_name,self.text,"\n\t",self.image_url,"\n\t",week_name,s_m,d,y)
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+                                                        
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timestamp = timestamp
 
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        
+        timestamp = datetime.timestamp(self.timestamp) # convert timestamp to integer before passing below
+        dt_object = datetime.fromtimestamp(timestamp)
+        week_name=get_week_day_name(dt_object.weekday())
+        m=dt_object.month
+        s_m =short_name(m)
+        d = dt_object.day
+        y = dt_object.year
+        
+        return '@{} Checked In: "{}"{}{}, {}{}{}, {} {}, {}'.format(self.user.first_name,self.text,"\n\t",self.latitude,self.longitude,"\n\t",week_name,s_m,d,y)
+    
